fa04e3ecb9fcd3bbcdcbcc110418b32b0ceb0a7c
Tobias Mansfield-Williams <contact@tmwhere.com>
2016-03-19 17:01:43 +0100

    Draw filled triangles (Lesson 2)

--- main.lua
+++ main.lua
@@ -36,11 +36,15 @@ function drawPoint(x, y, color)
     -- invert y-coordinate to set origin to bottom-left corner of screen
     rdr:drawPoint({x=x, y=HEIGHT-y})
 end
+ function round(v)
+     return math.floor(v+0.5)
+ end
 
-function line(x0, y0, x1, y1, color)
-    local function round(v)
-        return math.floor(v+0.5)
-    end
+function line(v0, v1, color)
+    local x0 = v0[1]
+    local y0 = v0[2]
+    local x1 = v1[1]
+    local y1 = v1[2]
     local xMag = math.abs(x1 - x0)
     local yMag = math.abs(y1 - y0)
     local useX = (xMag > yMag)
@@ -54,6 +58,64 @@ function line(x0, y0, x1, y1, color)
         drawPoint(x, y, color)
     end
 end
+
+function orderPointsBy(p0, p1, p2, dimIdx)
+    local lowP = nil
+    local midP = nil
+    local highP = nil
+    if p1[dimIdx] <= p0[dimIdx] and p0[dimIdx] <= p2[dimIdx] then
+        lowP = p1
+        midP = p0
+        highP = p2
+    elseif p2[dimIdx] <= p0[dimIdx] and p0[dimIdx] <= p1[dimIdx] then
+        lowP = p2
+        midP = p0
+        highP = p1
+    elseif p0[dimIdx] <= p1[dimIdx] and p1[dimIdx] <= p2[dimIdx] then
+        lowP = p0
+        midP = p1
+        highP = p2
+    elseif p2[dimIdx] <= p1[dimIdx] and p1[dimIdx] <= p0[dimIdx] then
+        lowP = p2
+        midP = p1
+        highP = p0
+    elseif p1[dimIdx] <= p2[dimIdx] and p2[dimIdx] <= p0[dimIdx] then
+        lowP = p1
+        midP = p2
+        highP = p0
+    elseif p0[dimIdx] <= p2[dimIdx] and p2[dimIdx] <= p1[dimIdx] then
+        lowP = p0
+        midP = p2
+        highP = p1
+    end
+    return lowP, midP, highP
+end
+
+function triangle(v0, v1, v2, color)
+    local yIdx = 2
+    local lowV, midV, highV = orderPointsBy(v0, v1, v2, yIdx)
+    local fracY = (midV[2] - lowV[2]) / (highV[2] - lowV[2])
+    local midVFrac = {lowV[1] + fracY * (highV[1] - lowV[1]), midV[2]}
+
+    for y = lowV[2], midV[2] do
+        local subFracY = (y - lowV[2]) / (midV[2] - lowV[2])
+        local x1 = round(lowV[1] + subFracY * (midVFrac[1] - lowV[1]))
+        local x2 = round(lowV[1] + subFracY * (midV[1] - lowV[1]))
+        for x = math.min(x1, x2), math.max(x1, x2) do
+            drawPoint(x, y, color)
+        end
+    end
+
+    for y = midV[2], highV[2] do
+        local subFracY = (y - midV[2]) / (highV[2] - midV[2])
+        local x1 = round(midVFrac[1] + subFracY * (highV[1] - midVFrac[1]))
+        local x2 = round(midV[1] + subFracY * (highV[1] - midV[1]))
+        for x = math.min(x1, x2), math.max(x1, x2) do
+            drawPoint(x, y, color)
+        end
+    end
+end
+
 
 function parseObj(path)
     local faces = {}
@@ -84,18 +146,16 @@ end
 
 local function drawModel(modelPath)
     local faces, verts = parseObj(modelPath)
+
+    function transformVToScreenSpace(v)
+        return {(v[1] + 1) * WIDTH / 2, (v[2] + 1) * HEIGHT / 2}
+    end
+
     for _, face in ipairs(faces) do
-        for i = 1, 3 do
-            local v0 = verts[face.verts[i]]
-            local v1 = verts[face.verts[i % 3 + 1]]
-            line(
-                (v0[1] + 1) * WIDTH / 2,      -- x0
-                (v0[2] + 1) * HEIGHT / 2,     -- y0
-                (v1[1] + 1) * WIDTH / 2,      -- x1
-                (v1[2] + 1) * HEIGHT / 2,     -- y1
-                {r=255, g=255, b=255, a=255}  -- color
-            )
-        end
+        local v0 = verts[face.verts[1]]
+        local v1 = verts[face.verts[2]]
+        local v2 = verts[face.verts[3]]
+        triangle(transformVToScreenSpace(v0), transformVToScreenSpace(v1), transformVToScreenSpace(v2), {r=255, g=255, b=255, a=255})
     end
 end
 
